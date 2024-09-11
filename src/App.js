4dd63ea73a31dc506cc0071dff567e0d5e111f12
19Sonali <109802850+19Sonali@users.noreply.github.com>
2024-09-11 10:21:55 +0530

    Created the side bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import LoginPage from "./Components/LoginPage";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
+import MainPage from "./Components/Main page/MainPage";
 
 
 function App() {
   return (
     <div>
-      <LoginPage/>
+      
+      <BrowserRouter>
+        {/* <Header/> */}
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route exact path="/Onebox" element={<MainPage />} />
+          
+        </Routes>
+        {/* <Footer/> */}
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Main page/SideBar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FaInbox } from "react-icons/fa";
+
+const SideBar = () => {
+  return (
+    <div className="bg-[#101113] h-screen w-14 flex flex-col justify-between items-center py-6 border-r-2 border-[#343A40] fixed z-10">
+      {/* logo */}
+      <div className="rounded-xl">
+        <img
+          className="h-8 rounded-xl object-left overflow-visible"
+          src="./images/logo.png"
+          alt=""
+        />
+      </div>
+      {/* icons */}
+      <div className="text-[#AEAEAE] text-2xl space-y-10 flex flex-col items-center mb-24">
+        <div>
+          <img src="./images/Home_fill.png" alt="" />
+        </div>
+        <div>
+          <img src="./images/email.png" alt="" />
+        </div>
+        <div>
+          <img src="./images/email (1).png" alt="" />
+        </div>
+        <div>
+          <img src="./images/Frame 23.png" alt="" />
+        </div>
+        <div>
+          <img src="./images/email (2).png" alt="" />
+        </div>
+        <div>
+          <FaInbox />
+        </div>
+        <div>
+          <img src="./images/bar_chart.png" alt="" />
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <img
+          src="https://via.placeholder.com/40"
+          alt="User Avatar"
+          className="w-8 h-8 rounded-full mb-2 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
